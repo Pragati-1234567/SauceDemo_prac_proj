abcbb5965c9303956d82a43808914aa46158a2c9
unknown <pragatirajas24@gmail.com>
2026-08-19 14:33:34 +0530

    new message

---
+++ tests/Demo1.spec.js
@@ -0,0 +1,14 @@
+const {test} = require('@playwright/test');
+
+test("Login sauce Demo", async ({page}) =>{
+
+    await page.goto("https://www.saucedemo.com/");
+
+    await page.locator("#user-name").fill("standard_user");
+
+    await page.locator("#password").fill("secret_sauce");
+
+    await page.getByText("Login").click();
+
+    await page.waitForTimeout(4000);
+})
